8752a8eab7b1423906105e2a1ed67b461a5bfe3b
iamsyahidi <ilhamsyahidi66@gmail.com>
2021-06-19 10:47:38 +0700

    Fix meta viewPort

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv="content-type" content="text/html;charset=UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
           <link rel="icon" type="image/png" href="favicon.ico" />
         </Head>
