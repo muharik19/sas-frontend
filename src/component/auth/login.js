8f2de525cd5a24e5ae354df7f1fbc840010c1972
muharik19 <ahmadmuharik@gmail.com>
2023-07-02 21:34:35 +0700

    [MODIFY] remove declaration not defined

--- src/component/auth/login.js
+++ src/component/auth/login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { Queries } from "../../config/graphql/index";
 import { useLazyQuery } from "@apollo/react-hooks";
